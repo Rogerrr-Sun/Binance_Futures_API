455052935e8fc905f2c706240a66229097331890
Roger <dsun.lajiyoujian@gmail.com>
2021-10-10 15:48:21 +0800

    update outdated examples

--- example_d/market/get_mark_price.py
+++ example_d/market/get_mark_price.py
@@ -4,7 +4,7 @@ from binance_d.base.printobject import *
 
 request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 
-result = request_client.get_mark_price(symbol="btcusd_200925")
+result = request_client.get_mark_price(symbol="BTCUSD_PERP")
 
 print("======= Mark Price =======")
 PrintMix.print_data(result)
